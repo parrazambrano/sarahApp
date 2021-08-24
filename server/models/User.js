8891d245026b39b7949f0bf73d85402d2e1ad305
parrazambrano <parrazambranod@gmail.com>
2021-08-24 10:43:06 -0700

    basic server framework built

---
+++ server/models/User.js
@@ -0,0 +1,59 @@
+const { Schema, model } = require("mongoose");
+
+const bcrypt = require("bcrypt");
+
+const userSchema = new Schema({
+  firstName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: [/.+@.+\..+/, "Must match an email address!"],
+    // validate: {
+    //   validator: () => Promise.resolve(false),
+    //   message: 'Email validation failed'
+    // }
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6,
+  },
+  parkingPlace: [{ type: Schema.Types.ObjectId, ref: "ParkingPlace" }],
+  bookings: [{ type: Schema.Types.ObjectId, ref: "Reservation" }],
+});
+
+// set up pre-save middleware to create password
+userSchema.pre(
+  "save",
+  async function (next) {
+    if (this.isNew || this.isModified("password")) {
+      const saltRounds = 10;
+      this.password = await bcrypt.hash(this.password, saltRounds);
+    }
+    next();
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
+
+// compare the incoming password with the hashed password
+userSchema.methods.isCorrectPassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+const User = model("User", userSchema);
+
+module.exports = User;
---
+++ server/schemas/resolvers.js
@@ -0,0 +1,73 @@
+const {
+  AuthenticationError
+} = require("apollo-server-express");
+const {
+  User
+} = require("../models");
+const {
+  signToken
+} = require("../utils/auth");
+
+const resolvers = {
+  Query: {
+    user: async (parent, args, context) => {
+      if (context.user) {
+        const userdata = await User.findById({
+            _id: context.user._id
+          })
+          .select("-__v -password")
+        return userdata;
+      }
+
+      throw new AuthenticationError("Not logged in");
+    },
+  },
+    Mutation: {
+      addUser: async (parent, args) => {
+        const user = await User.create(args);
+        const token = signToken(user);
+
+        return {
+          token,
+          user
+        };
+      },
+
+      editUser: async (parent, args, context) => {
+        const user = await User.findByIdAndUpdate({
+            _id: context.user._id
+          },
+          args, {
+            new: true
+          }
+        );
+        return user;
+      },
+
+      login: async (parent, {
+        email,
+        password
+      }) => {
+        const user = await User.findOne({
+          email
+        });
+        if (!user) {
+          throw new AuthenticationError("Incorrect credentials");
+        }
+        const correctPw = await user.isCorrectPassword(password);
+
+        if (!correctPw) {
+          throw new AuthenticationError("Incorrect credentials");
+        }
+
+        const token = signToken(user);
+
+        return {
+          token,
+          user
+        };
+      },
+    },
+};
+
+module.exports = resolvers;
