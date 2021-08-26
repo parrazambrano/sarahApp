3efc69db8c970026cc376591f57dca6351f04807
parrazambrano <parrazambranod@gmail.com>
2021-08-26 15:07:08 -0700

    added buttons to main menu and began styling

--- client/src/pages/Home/index.jsx
+++ client/src/pages/Home/index.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 // import Login from '../../components/Login'
 import Nav from '../../components/Nav'
+import './style.css'
 
 const Home = () => {
     return (
         <>
           <Nav />
-          <div className='heroSection'></div>
+          <div className='mainMenu'>
+              <button className='mainMenuBtn announcementsBtn'></button>
+              <button className='mainMenuBtn myMessagesBtn'></button>
+              <button className='mainMenuBtn messageBoardBtn'></button>
+              <button className='mainMenuBtn settingsBtn'></button>
+          </div>
         </>
     )
 }
