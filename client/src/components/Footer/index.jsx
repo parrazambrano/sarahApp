1f63a742551558ffa7997d5349e92382bd77f174
parrazambrano <parrazambranod@gmail.com>
2021-09-16 12:38:53 -0700

    changed footer icons and header icon as well as styling.

--- client/src/components/Footer/index.jsx
+++ client/src/components/Footer/index.jsx
@@ -2,34 +2,37 @@ import React from 'react';
 import { Link } from "react-router-dom";
 import './style.css';
 
-import messageBoardPng from './images/coworking.png'
-import myMessagesPng from './images/email.png'
-import announcementPng from './images/megaphone.png'
+import homePng from './images/home.png'
+import createPostPng from './images/add.png'
+import announcementPng from './images/bell.png'
 import settingsPng from './images/settings.png'
 
 const Home = () => {
     return (
         <>
             <div className='mainMenu'>
-                <Link className='mainMenuBtnCase' to='/announcements'>
-                    <button className='mainMenuBtn announcementsBtn'>
-                        <img className='mainMenuBtnPng' src={announcementPng} alt="announcements" />
-                    </button>
-                </Link>
-                <Link className='mainMenuBtnCase' to='/my-messages'>
-                    <button className='mainMenuBtn myMessagesBtn'>
-                        <img className='mainMenuBtnPng' src={myMessagesPng} alt="private messages" />
-                    </button>
-                </Link>
                 <Link className='mainMenuBtnCase' to='/message-board'>
-                    <button className='mainMenuBtn messageBoardBtn'>
-                        <img className='mainMenuBtnPng' src={messageBoardPng} alt="team message board" />
-                    </button>
+                    <div className='mainMenuBtn messageBoardBtn'>
+                        <img className='mainMenuBtnPng' src={homePng} alt="team message board" />
+                    </div>
                 </Link>
+
+                <Link className='mainMenuBtnCase' to='/my-messages'>
+                    <div className='mainMenuBtn myMessagesBtn'>
+                        <img className='mainMenuBtnPng' src={createPostPng} alt="private messages" />
+                    </div>
+                </Link>
+
+                <Link className='mainMenuBtnCase' to='/announcements'>
+                    <div className='mainMenuBtn announcementsBtn'>
+                        <img className='mainMenuBtnPng' src={announcementPng} alt="announcements" />
+                    </div>
+                </Link>
+
                 <Link className='mainMenuBtnCase' to='/settings'>
-                    <button className='mainMenuBtn settingsBtn'>
+                    <div className='mainMenuBtn settingsBtn'>
                         <img className='mainMenuBtnPng' src={settingsPng} alt="settings" />
-                    </button>
+                    </div>
                 </Link>
             </div>
         </>
--- client/src/components/Nav/index.jsx
+++ client/src/components/Nav/index.jsx
@@ -3,29 +3,33 @@ import { Link } from "react-router-dom";
 import { OPEN_MENU } from "../../utils/actions";
 import { useStoreContext } from "../../utils/GlobalState";
 import logo from './images/sabre_logo.png';
-import menu from './images/menu.png';
+import messenger from './images/messenger.png';
 import './style.css';
-import Auth from "../../utils/auth";
+// import Auth from "../../utils/auth";
 
 const Nav = () => {
 
-    const [state, dispatch] = useStoreContext();
+    // const [state, dispatch] = useStoreContext();
     
-    const handleMenu = () => {
-        dispatch({
-                  type: OPEN_MENU,
-                  menu: !state.menu
-                })
-    }
+    // const handleMenu = () => {
+    //     dispatch({
+    //               type: OPEN_MENU,
+    //               menu: !state.menu
+    //             })
+    // }
 
     return (<>
         <header>
             <Link to='/'>
                 <img className='sabreLogo' src={logo} alt="Sabre Logo" />
             </Link>
-            <button className='menuDropBtn' onClick={handleMenu}><img className='menuDropPng' src={menu} alt=""/></button>
+            {/* <div className='menuDropBtn' onClick={handleMenu}> */}
+            <Link to='my-messages'>
+            <img className='messagePng' src={messenger} alt="messages"/>
+            </Link>
+            {/* </div> */}
         </header>
-        <div className='dropDown'>
+        {/* <div className='dropDown'>
         {state.menu && <div className='menuItemContainer'>
         {Auth.loggedIn() ? 
             <div onClick={() => Auth.logout()} className='menuItem'>Log-out</div>
@@ -34,9 +38,9 @@ const Nav = () => {
             <div onClick={() => console.log('clicked')} className='menuItem'>item</div>
             <div onClick={() => console.log('clicked')} className='menuItem'>item</div>
         </div>}
-        </div>
+        </div> */}
         </>
     )
 }
 
-export default Nav
+export default Nav;
