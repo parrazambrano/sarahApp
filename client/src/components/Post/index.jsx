c57593e218caeeb19a4341b733379f43af1026e8
parrazambrano <parrazambranod@gmail.com>
2021-11-21 06:42:16 -0800

    used bootstraps "cards" to display announcements

--- client/src/components/Post/index.jsx
+++ client/src/components/Post/index.jsx
@@ -1,16 +1,21 @@
 import React from 'react'
 
 const Post = (props) => {
-    const {content, title,user,whatGym } = props.props;
-    
+    const { content, title, user, whatGym } = props.props;
+    console.log(props);
     return (
-        <div className='postBody'>
-        <h1>{title}</h1>
-        <p>{content}</p>
-        <p>{whatGym}</p>
-        <p>{user}</p>
+        <div className="card mb-3">
+            {/* <img src="..." class="card-img-top" alt="..." /> */}
+            <div class="card-body">
+                <h5 class="card-title">{title}</h5>
+                <p class="card-text">{content}</p>
+                <p class="card-text"><small class="text-muted">{whatGym}</small></p>
+                <p class="card-text"><small class="text-muted">{user.username}</small></p>
+            </div>
         </div>
+
     )
 }
 
 export default Post
+
--- client/src/pages/Announcements/index.jsx
+++ client/src/pages/Announcements/index.jsx
@@ -7,12 +7,12 @@ const Announcements = () => {
     const { loading, data: postData } = useQuery(QUERY_ALL_POSTS);
     const { loading: loadingUser, data: userData } = useQuery(QUERY_USER);
     
-    console.log(postData)
+    console.log(postData.getAllPosts.reverse())
     console.log(userData)
 
     return <div>
     { loading && <h1>LOADING</h1> }
-    {/* {postData && postData.getAllPosts.slice(0).reverse().map((post, index) => post.announcement && <Post props={post} key={index}/>)} */}
+    {postData && postData.getAllPosts.slice(0).reverse().map((post, index) => post.announcement && <Post props={post} key={index}/>)}
     Hello World
     </div>
 }
