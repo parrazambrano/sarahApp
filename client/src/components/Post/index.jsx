d011070021043b735728fb8e8f521e09d40d14c3
parrazambrano <parrazambranod@gmail.com>
2021-11-25 11:19:20 -0800

    restyled Post component

--- client/src/components/Post/index.jsx
+++ client/src/components/Post/index.jsx
@@ -1,12 +1,12 @@
 import React, { useState, } from 'react';
-import { Card, Form, Button } from 'react-bootstrap'
+import { Card, Form, Button, FloatingLabel } from 'react-bootstrap'
 import { useStoreContext } from "../../utils/GlobalState";
 
 
 const Post = (props) => {
     const { content, title, user, whatGym } = props.props;
     const [chat, setChat] = useState(false)
-    const [commentState, setCommentState] = useState({ comment: ""});
+    const [commentState, setCommentState] = useState({ comment: "" });
     // const [state, dispatch] = useStoreContext();
 
     const handleChange = event => {
@@ -21,6 +21,16 @@ const Post = (props) => {
         // console.log(state.currentUser.username)
     }
 
+    const handleSelect = e => {
+        e.target.style.height = 'inherit';
+        e.target.style.height = `${e.target.scrollHeight}px`;
+        setChat(true)
+    }
+    const handleBlur = e => {
+        e.target.style.height = '9vh';
+        setChat(false)
+    }
+
     return (<>
         <Card className='m-1'>
             <Card.Header as='h5'>{title}</Card.Header>
@@ -30,17 +40,29 @@ const Post = (props) => {
                 <Card.Text><small className="text-muted">{user.username}</small></Card.Text>
             </Card.Body>
             {/* <Card.Img variant="bottom" src="holder.js/100px180" /> */}
-            <div className="comment-button-container m-1">
+            {/* <div className="comment-button-container m-1">
                 {!chat && <Button onClick={() => { setChat(true) }}>Comment</Button>}
-            </div>
+            </div> */}
+            <FloatingLabel controlId="floatingTextarea2" label='Comment'>
+                <Form.Control
+                    as="textarea"
+                    placeholder='Comment'
+                    style={{ height: '9vh',
+                    width: '95vw' }}
+                    onSelect={handleSelect}
+                    onBlur={handleBlur}
+                    onChange={handleChange}
+                    className='m-1'
+                />
+            </FloatingLabel>
 
             {chat && <Form className='m-1'>
-                <Form.Group className='mb-1'>
+                {/* <Form.Group className='mb-1'>
                     <Form.Label>Comment</Form.Label>
                     <Form.Control as="textarea" onChange={handleChange} value={commentState.comment} rows={3} />
-                </Form.Group>
+                </Form.Group> */}
                 <Button onClick={handleCommentSubmit} variant="primary" type="button">
-                    Submit
+                    Send
                 </Button>
             </Form>}
         </Card>
