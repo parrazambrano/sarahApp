954e2cd331107cd3ac8630b9aede10e3188dcee3
parrazambrano <parrazambranod@gmail.com>
2022-01-12 10:41:26 -0800

    added title to announcement page

--- client/src/pages/Announcements/index.jsx
+++ client/src/pages/Announcements/index.jsx
@@ -1,25 +1,36 @@
-import React, {useEffect} from 'react';
-import Post from '../../components/Post';
-import { useQuery } from "@apollo/react-hooks";
-import { QUERY_ALL_POSTS } from '../../utils/queries';
-import { NEW_ANNOUNCEMENT } from "../../utils/actions";
-import { useStoreContext } from "../../utils/GlobalState";
+import React, { useEffect } from 'react'
+import Post from '../../components/Post'
+import { useQuery } from '@apollo/react-hooks'
+import { QUERY_ALL_POSTS } from '../../utils/queries'
+import { NEW_ANNOUNCEMENT } from '../../utils/actions'
+import { useStoreContext } from '../../utils/GlobalState'
+import './style.css'
 
 const Announcements = () => {
-    const { loading: postLoading, data: postData } = useQuery(QUERY_ALL_POSTS);
-    const[,dispatch] = useStoreContext()
+  const { loading: postLoading, data: postData } = useQuery(QUERY_ALL_POSTS)
+  const [, dispatch] = useStoreContext()
 
-useEffect(() => {
+  useEffect(() => {
     dispatch({
-            type: NEW_ANNOUNCEMENT,
-            newAnnouncement: false
-        })
-}, [dispatch])
+      type: NEW_ANNOUNCEMENT,
+      newAnnouncement: false,
+    })
+  }, [dispatch])
 
-    return <div>
-        {postLoading && <h1>LOADING</h1>}
-        {postData && postData.getAllPosts.slice(0).reverse().map((post, index) => post.announcement && <Post props={post} key={index} />)}
+  return (
+    <div>
+      <h1 className='announcementTitle'>Team Announcements:</h1>
+      {postLoading && <h1>LOADING</h1>}
+      {postData &&
+        postData.getAllPosts
+          .slice(0)
+          .reverse()
+          .map(
+            (post, index) =>
+              post.announcement && <Post props={post} key={index} />,
+          )}
     </div>
-};
+  )
+}
 
 export default Announcements
