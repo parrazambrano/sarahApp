12160b074eff6c3d0a0801a63df6415a975486d6
parrazambrano <parrazambranod@gmail.com>
2022-03-28 16:47:28 -0600

    added table for tracking user log in dates

--- client/src/pages/TrainingLog/index.jsx
+++ client/src/pages/TrainingLog/index.jsx
@@ -1,13 +1,68 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useQuery } from '@apollo/client'
 import { QUERY_ALL_CHECKINS } from '../../utils/queries'
+import { useStoreContext } from '../../utils/GlobalState'
+import { Table, Form } from 'react-bootstrap'
 
 const TrainingLog = () => {
-    const { data, } = useQuery(QUERY_ALL_CHECKINS);
-    console.log(data);
+  const [state] = useStoreContext()
+  const [memberTable, setMemberTable] = useState(null)
+  const { data } = useQuery(QUERY_ALL_CHECKINS)
+
+  useEffect(() => {
+    data && setMemberTable(data.getCheckIn)
+  }, [data])
+
+  useEffect(() => {
+    state.currentUser &&
+      !state.currentUser.administrator &&
+      setMemberTable(memberTable.filter(
+          (user) => {return (user.user._id === state.currentUser._id)},
+        ),);
+  }, [state.currentUser, setMemberTable])
+
+  const gymFilter = (event) => {
+    const { value } = event.target
+    if (value === 'default') {
+      setMemberTable(data.getCheckIn)
+    } else {
+      setMemberTable(data.getCheckIn.filter((user) => user.gym === value))
+    }
+  }
   return (
     <>
-        {data && data.getCheckIn.map((e)=> e.user && console.log(e.user.username))}
+      {state.currentUser && state.currentUser.administrator && (
+        <Form.Select onChange={gymFilter} name="whatGym">
+          <option value="default">Filter Gym</option>
+          <option value="sabre">Sabre</option>
+          <option value="T4L">T4L</option>
+          <option value="eastbayacademy">East Bay Academy</option>
+          <option value="dumlaos">Dumlao's</option>
+          <option value="bigbreak">Big Break</option>
+          <option value="rooted">Rooted</option>
+        </Form.Select>
+      )}
+      <Table>
+        <thead>
+          <tr>
+            <th>User</th>
+            <th>Date</th>
+            <th>Gym</th>
+          </tr>
+        </thead>
+        <tbody>
+          {memberTable &&
+            memberTable.map((user, index) => {
+              return (
+                <tr key={index}>
+                  <td>{user.user.username}</td>
+                  <td>{user.date}</td>
+                  <td>{user.gym}</td>
+                </tr>
+              )
+            })}
+        </tbody>
+      </Table>
     </>
   )
 }
--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -24,6 +24,7 @@ export const QUERY_ALL_CHECKINS = gql `
     user {
       _id
       username
+      paid
     }
     date 
   }
